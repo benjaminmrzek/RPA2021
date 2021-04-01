664f68d15dc07810589a443edb7a99d0825cf689
Benjamin <benjamin.mrzek@gmail.com>
2021-04-01 14:28:21 +0200

    Dodajanje viewu

---
+++ Vaja/Vaja/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vaja.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // HelloWorld/Pozdravljen?ime=Ben&num=2
+        public ActionResult Pozdravljen(string ime, int num=1)
+        {
+            ViewBag.Message = "Pozdravljen " + ime;
+            ViewBag.Num = num;
+            return View();
+        }
+    }
+}
