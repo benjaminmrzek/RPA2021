2827dc2baafb2328a673aea2a00388f3f6031d8d
Benjamin <benjamin.mrzek@gmail.com>
2021-04-23 10:01:45 +0200

    Dodajanje WebAPI vaje

--- TrgovinaWebAPI/TrgovinaWebAPI/Controllers/HomeController.cs
+++ TrgovinaWebAPI/TrgovinaWebAPI/Controllers/HomeController.cs
@@ -8,6 +8,12 @@ namespace TrgovinaWebAPI.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Admin() //dodano
+        {
+            string apiUri = Url.HttpRouteUrl("DefaultApi", new { controller = "admin" }); //Pazimo da je isti kot na WebApiConfig.cs
+            ViewBag.ApiUrl = new Uri(Request.Url, apiUri).AbsoluteUri.ToString();
+            return View();
+        }
         public ActionResult Index()
         {
             return View();
---
+++ TrgovinaWebAPI/TrgovinaWebAPI/Data/TrgovinaWebAPIContextInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using TrgovinaWebAPI.Models;
+
+namespace TrgovinaWebAPI.Data
+{
+    public class TrgovinaWebAPIContextInitializer:DropCreateDatabaseIfModelChanges<TrgovinaWebAPIContext>
+    {
+        protected override void Seed(TrgovinaWebAPIContext context)
+        {
+            var p = new List<Product>()
+            {
+                new Product(){Name="Juha",Price=1.39M,ActualCost=0.99m},
+                new Product(){Name="Kladivo",Price=16.99M,ActualCost=10},
+                new Product(){Name="Žoga",Price=6.99M,ActualCost=2.05m}
+            };
+            foreach (var x in p)
+                context.Products.Add(x);
+            // p.ForEach(x => context.Products.Add(x)); //enaka stvar
+            context.SaveChanges();
+
+            var order = new Order() { Customer = "Bob" };
+            var od = new List<OrderDetail>()
+            {
+                new OrderDetail(){Product=p[0],Quantity=2,Order=order},
+                new OrderDetail(){Product=p[1],Quantity=4,Order=order}
+            };
+            context.Orders.Add(order);
+            od.ForEach(y => context.OrderDetails.Add(y));
+            context.SaveChanges();
+        }
+    }
+}
